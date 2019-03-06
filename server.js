b2493699d0cba7dc88cbd6cf293294d6d3f04912
Kilian Stallinger <kp.stallinger@gmail.com>
2019-03-06 17:52:34 +0100

    Change Metadata

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use('/', serveStatic ( path.join (__dirname, '/dist') ) )
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html')
+})
 const port = process.env.PORT || 80
 app.listen(port)
 console.log('App started')
